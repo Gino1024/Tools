d05e8555957a2d258931c36414e7f6960abfbd23
unknown <p10376579@pic.com.tw>
2024-01-04 17:11:29 +0800

    feat: deploy .net framework Batch

---
+++ DeployBatch/DeployBatchMain.bat
@@ -0,0 +1,41 @@
+setlocal enabledelayedexpansion
+set LANG=en-US
+set default_import_path=C:\1.Dev\
+
+for /f "tokens=1-9 delims=/:. " %%a in ("%date% %time%") do (
+  set year=%%a
+  set month=%%b
+  set day=%%c
+  set hour=%%e
+  set minute=%%f
+  set second=%%g
+)
+
+set datetime=%year%_%month%_%day%_%hour%_%minute%_%second%
+set default_export_path=%CD%\%datetime%\
+mkdir %default_export_path%
+
+for /f %%a in (.\list.txt) do (
+    set sys=%%a
+    set sys=!sys:~0,3!
+	set importPath=%default_import_path%\!sys!\%%a
+	set exportPath=%default_export_path%\!sys!\%%a
+	
+	
+	echo %date% %time% : %%a build >> %default_export_path%\log.txt
+	echo dotnet build !importPath! -c Debug -o !exportPath! >> %default_export_path%\log.txt
+	dotnet build !importPath! -c Debug -o !exportPath! | find "error" >> %default_export_path%\log.txt
+	
+	if not errorlevel 1 (
+	
+	    del /Q /F !exportPath!\*
+	    echo %date% %time% : %%a Failed. >> %default_export_path%\log.txt
+	) else (
+	
+	    echo %date% %time% : %%a remove config.. >> %default_export_path%\log.txt
+	    del !exportPath!\*.config
+	    echo %date% %time% : %%a Sucess. >> %default_export_path%\log.txt
+	)
+
+
+)
---
+++ DeployBatch/readme.md
@@ -0,0 +1,5 @@
+將list清單對應位置csproj做建置並輸出到當前目錄
+取每一row檔名前三碼當第二層目錄
+
+ex: ECA103
+$"{default_import_path}\ECA\ECA103"
